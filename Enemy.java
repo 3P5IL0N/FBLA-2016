432102b1c256de2264e88058f035e5298189d041
3P5IL0N <railrider1@verizon.net>
2016-01-27 14:11:41 -0500

    Add Enemy Class
    Work in progress

---
+++ Enemy.java
@@ -0,0 +1,18 @@
+import java.awt.Graphics;
+
+public class Enemy extends AbstractObject {
+
+	public Enemy() {
+		 myX = 200;
+	     myY = 200;
+	     myDeltaX=0;
+	     myDeltaY=0;
+	}
+
+	@Override
+	public void draw(Graphics myBuffer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
